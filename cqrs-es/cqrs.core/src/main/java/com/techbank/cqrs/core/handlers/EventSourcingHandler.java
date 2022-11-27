3e349afdbefdcb482716b71dd981808f504a8537
Evan Yang <evan.yang@sjsu.edu>
2022-11-27 15:57:22 -0800

    create restoreDatabase function and add different database

--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/handlers/EventSourcingHandler.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/handlers/EventSourcingHandler.java
@@ -4,6 +4,6 @@ import com.techbank.cqrs.core.domain.AggregateRoot;
 
 public interface EventSourcingHandler<T> {
     void save(AggregateRoot aggregate);
-
     T getById(String id);
+    void republishEvents();
 }
